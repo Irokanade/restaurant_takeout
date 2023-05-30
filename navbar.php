50d473718b8ac3c2d9f1e970c27868ee08ca9f6f
yum <46490506+Kleefia@users.noreply.github.com>
2023-05-31 02:46:08 +0800

    Add files via upload

--- navbar.php
+++ navbar.php
@@ -29,6 +29,28 @@
         <li><a class="active" href="restaurants.php">Home</a></li>
         <li><a href="myOrders.php">My Orders</a></li>
         <li><a href="myCart.php">My Cart</a></li>
-        <li><a class="active" href="logout.php">Logout</a></li>
+
+        <?php
+        if(!isset($_SESSION['login_user'])){
+            echo '<li><a class="active" href="login.php">Login';
+        } else {
+            $sql = "SELECT user_name FROM login_cred WHERE login_id = '$login_session'";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                $user_name = $row["user_name"];
+                if($user_name == NULL){
+                    echo '<li><a class="active" href="login.php">Login';
+                } else {
+                    echo '<li><a href="userpage.php">Hi ';
+                    echo $user_name;
+                    echo '</a></li>';
+                    echo '<li><a class="active" href="logout.php">Logout';
+                }
+            }   
+        }
+        
+        ?></a></li>
+        
     </ul>
 </nav>
